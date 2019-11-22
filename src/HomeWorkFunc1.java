ce4af8a3f7ecc04b358fb6e6a0e573ecc3665cbf
Назар Садик <Slameryt4@gmail.com>
2019-11-22 18:32:45 +0200

    HomeWork

---
+++ src/HomeWorkFunc1.java
@@ -0,0 +1,70 @@
+public class HomeWorkFunc1 {
+    private int first;
+    private int first2;
+    private int second;
+    private int second2;
+    private float number;
+
+    public HomeWorkFunc1(int first, int first2, int second, int second2, float number) {
+        this.first = first;
+        this.first2 = first2;
+        this.second = second;
+        this.second2 = second2;
+        this.number = number;
+    }
+
+    public int getFirst() {
+        return first;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public float getNumber() {
+        return number;
+    }
+
+    public int getFirst2() {
+        return first2;
+    }
+
+    public int getSecond2() {
+        return second2;
+    }
+
+    public void Read(){
+        getFirst();
+        getFirst2();
+        getSecond();
+        getSecond2();
+        getNumber();
+    }
+
+    public void rangeCheck(){
+
+        int resLeft = getFirst()/getSecond2();
+        int resRight = getSecond()/getSecond2();
+
+        if (number>resLeft && number<resRight){
+            System.out.println("Вы прошли проверку");
+        }else {
+            System.out.println("Вы не прошли проверку");
+        }
+    }
+
+    public void Display(){
+        System.out.print("Первая дробь: " + getFirst() + "/" + getFirst2());
+        System.out.println();
+        System.out.println("Вторая дробь: " + getSecond() + "/" + getSecond2());
+        rangeCheck();
+    }
+
+//    public void PrintFirst(){
+//        System.out.println(getFirst()+"/" + getFirst2());
+//    }
+//
+//    public void PrintSecond(){
+//        System.out.println(getSecond()+"/"+ getSecond2());
+//    }
+}
---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите первую дробь");
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        System.out.println("Введите вторую дробь");
+        int c = in.nextInt();
+        int d = in.nextInt();
+
+        System.out.println("Введите число, которое будет выполнять проверку");
+        float n = in.nextFloat();
+
+
+        HomeWorkFunc1 homeWorkFunc1 = new HomeWorkFunc1(a,b,c,d,n);
+
+        homeWorkFunc1.Read();
+
+        homeWorkFunc1.Display();
+
+
+
+
+
+
+
+
+//        double a = in.nextDouble();
+//        double b = in.nextDouble();
+//        double c = in.nextDouble();
+//        Triangle triangle = new Triangle(a,b,c);
+//        System.out.println("Создан треугольник со сторонами a="
+//        + triangle.getA()+ " b="+triangle.getB()+ " c="+triangle.getC());
+//        System.out.println("Площадь "+triangle.getArea());
+//        System.out.println("Периметр "+triangle.getPerimetr());
+//
+    }
+}
